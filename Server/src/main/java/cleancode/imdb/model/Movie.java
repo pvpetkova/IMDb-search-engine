8f6fc4e7602a01ff9f072cdf50aa9ed1f169d566
pvpetkova <52152313+pvpetkova@users.noreply.github.com>
2019-07-01 22:47:13 +0300

    added allargs constructor to Movie

--- Server/src/main/java/cleancode/imdb/model/Movie.java
+++ Server/src/main/java/cleancode/imdb/model/Movie.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -19,6 +21,15 @@ public class Movie extends Listing {
     private String website;
     @JsonProperty(value = "Response")
     private String response;
+
+    public Movie(String title, String year, String rated, String released, String runtime, String genre, String director, String writer, String actors, String plot, String language, String country, String awards, String poster, List<Rating> ratings, String metascore, String imdbRating, String imdbVotes, String imdbId, String type, String dvd, String boxOffice, String production, String website, String response) {
+        super(title, year, rated, released, runtime, genre, director, writer, actors, plot, language, country, awards, poster, ratings, metascore, imdbRating, imdbVotes, imdbId, type);
+        this.dvd = dvd;
+        this.boxOffice = boxOffice;
+        this.production = production;
+        this.website = website;
+        this.response = response;
+    }
 
     @Override
     public String toString() {
